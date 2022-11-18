2be25f3337e76f7f018afcb213962a28d326286e
MZ <gamboox@gmail.com>
2022-11-18 21:15:02 +0100

    Add navbar

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,14 @@
 import { Application } from "@hotwired/stimulus"
+import Alpine from "alpinejs"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+// Consigure Alpine.js
+window.Alpine = Alpine
+Alpine.start()
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
+pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.10.3/dist/module.esm.js"
 pin "lightweight-charts", to: "https://ga.jspm.io/npm:lightweight-charts@3.8.0/dist/lightweight-charts.esm.production.js"
 pin "fancy-canvas/coordinate-space", to: "https://ga.jspm.io/npm:fancy-canvas@0.2.2/coordinate-space.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
